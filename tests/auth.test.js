240acc965d83b204f931dcb51c80c5b815e144d2
thangs2024 <slmthang2024@gmail.com>
2024-06-11 18:01:12 -0400

    refactored few lines

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -14,30 +14,134 @@ const User = require('../mongoDB/models').User;
 
 const api = supertest(app);
 
-beforeEach(async () => {
-    // delete everything
-    await User.deleteMany({});
 
-    // init users
-    let users = await auth_test_helper.initUsers();
-    users = users.map( user =>  new User(user) );
+describe('testing...', () => {
 
-    const promiseArray = users.map(user => user.save());
-    await Promise.all(promiseArray);
+    beforeEach(async () => {
+        // delete everything
+        await User.deleteMany({});
+    
+        // init users
+        let users = await auth_test_helper.initUsers();
+        users = users.map( user =>  new User(user) );
+    
+        const promiseArray = users.map(user => user.save());
+        await Promise.all(promiseArray);
+    })
+
+    describe('auth tests', () => {
+
+        test('user authentication process check.', async () => {
+
+            const user = {
+                username: 'Luffy',
+                password: "123"
+            }
+        
+            await api
+                .post('/auth/login')
+                .send(user)
+                .expect(200)
+                .expect('Content-Type', /application\/json/);
+        
+        })
+
+    })
+
+    describe('api tests', () => {
+
+        describe('fetching data', () => {
+
+            describe('fetch all', () => {
+
+                test('fetch all events', async () => {
+
+                    await api
+                        .get('/api/events/all')
+                            .expect(200)
+                            .expect('Content-Type', /application\/json/);
+                })
+    
+                test('fetch all places', async () => {
+    
+                    await api
+                        .get('/api/places/all')
+                            .expect(200)
+                            .expect('Content-Type', /application\/json/);
+                })
+    
+                test('fetch all hotels', async () => {
+    
+                    await api
+                        .get('/api/hotels/all')
+                            .expect(200)
+                            .expect('Content-Type', /application\/json/);
+                })
+    
+                test('fetch all restaurants', async () => {
+    
+                    await api
+                        .get('/api/restaurants/all')
+                            .expect(200)
+                            .expect('Content-Type', /application\/json/);
+                })
+
+            })
+
+            describe('fetch by count', () => {
+                
+                test('fetch by count ( events )', async() => {
+
+                    await api
+                            .get('/api/events/count')
+                            .expect(200)
+                            .expect('Content-Type', /application\/json/)
+                            .expect( (response) => {
+                                assert(response.body.hasOwnProperty('count'))
+                            });
+
+                })
+
+                test('fetch by count ( places )', async() => {
+
+                    await api
+                            .get('/api/places/count')
+                            .expect(200)
+                            .expect('Content-Type', /application\/json/)
+                            .expect( (response) => {
+                                assert(response.body.hasOwnProperty('count'))
+                            });
+
+                }) 
+
+                test('fetch by count ( hotels )', async() => {
+
+                    await api
+                            .get('/api/hotels/count')
+                            .expect(200)
+                            .expect('Content-Type', /application\/json/)
+                            .expect( (response) => {
+                                assert(response.body.hasOwnProperty('count'))
+                            });
+
+                }) 
+
+                test('fetch by count ( restaurants )', async() => {
+
+                    await api
+                            .get('/api/restaurants/count')
+                            .expect(200)
+                            .expect('Content-Type', /application\/json/)
+                            .expect( (response) => {
+                                assert(response.body.hasOwnProperty('count'))
+                            });
+
+                }) 
+
+            })
+
+        })
+    })
+
 })
 
-
-test.only('user authentication process check.', async () => {
-
-    const user = {
-        username: 'Luffy',
-        password: "123"
-    }
-
-    await api
-        .post('/auth/login')
-        .send(user)
-        .expect(200)
-        .expect('Content-Type', /application\/json/);
-
-})
